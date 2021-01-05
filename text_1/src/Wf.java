27663bce811c9308b8543853b5f67f00833c9306
wf <1611146515@qq.com>
2021-01-05 14:29:25 +0800

    主分支修改wf（获取年龄）

--- text_1/src/Wf.java
+++ text_1/src/Wf.java
@@ -2,10 +2,15 @@ public class Wf {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaa");
         text();
+        getAge();
     }
 
     public static void text(){
         System.out.println("text----------------");
         System.out.println("11111111111111111111111111111111111111");
+    }
+
+    public static int getAge(){
+        return 3+18;
     }
 }
