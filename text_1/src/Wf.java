684517059fba45bfcc9d3e6b582c5124f3ff7f3e
wf <1611146515@qq.com>
2021-01-05 14:28:28 +0800

    分支修改wf（获取名字）

--- text_1/src/Wf.java
+++ text_1/src/Wf.java
@@ -2,10 +2,15 @@ public class Wf {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaa");
         text();
+        getName();
     }
 
     public static void text(){
         System.out.println("text----------------");
         System.out.println("11111111111111111111111111111111111111");
+    }
+
+    public static String getName(){
+        return "张三";
     }
 }
