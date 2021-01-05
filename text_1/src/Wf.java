6fdf02c723f88f5793eee258f032211b10ee7150
wf <1611146515@qq.com>
2021-01-05 11:38:33 +0800

    修改wf文件内容

--- text_1/src/Wf.java
+++ text_1/src/Wf.java
@@ -1,5 +1,10 @@
 public class Wf {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaa");
+        text();
+    }
+
+    public static void text(){
+        System.out.println("text----------------");
     }
 }
